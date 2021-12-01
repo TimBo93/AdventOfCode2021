97a6294f6d6bac7cce41ce11bdef786164c39531
Tim Borowski <tim@borowski-software.de>
2021-12-02 00:58:35 +0100

    Add project files.

---
+++ AdventOfCode2021/Day 1/Day1.cs
@@ -0,0 +1,23 @@
+﻿using var streamReader = File.OpenText("Day 1/input.txt");
+var lines = new List<string>();
+
+while(true)
+{
+    var line = await streamReader.ReadLineAsync();
+    if(line == null)
+    {
+        break;
+    }
+    lines.Add(line);
+}
+
+var count = lines.Select(x => int.Parse(x)).ToArray().GetSlidingWindow(2).Where(x => x[0] < x[1]).Count();
+var count2 = lines
+    .Select(x => int.Parse(x)).ToArray()
+    .GetSlidingWindow(3).Select(x => x.Sum()).ToArray()
+    .GetSlidingWindow(2).Where(x => x[0] < x[1])
+    .Count();
+
+
+Console.WriteLine($"the part1 count is {count}");
+Console.WriteLine($"the part2 count is {count2}");
---
+++ AdventOfCode2021/Day 1/SlidingWindowExtension.cs
@@ -0,0 +1,32 @@
+﻿
+
+// calculate increments
+
+
+public static class SlidingWindowExtension
+{
+    public static IEnumerable<T[]> GetSlidingWindow<T>(this T[] items, int count)
+    {
+        if(count <=0)
+        {
+            throw new ArgumentException($"{nameof(count)} must be greater 0.");
+        }
+        if(items.Count() < count)
+        {
+            yield return Array.Empty<T>();
+            yield break;
+        }
+        
+        for(var i=0; i<items.Count()-(count - 1); i++)
+        {
+            var slidingWindow = new List<T>();
+            for(var j=0; j<count; j++)
+            {
+                slidingWindow.Add(items[i + j]);
+            }
+            yield return slidingWindow.ToArray();
+        }
+    }
+} 
+
+
